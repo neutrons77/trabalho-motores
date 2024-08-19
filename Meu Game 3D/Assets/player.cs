600ee2737ff4dde022ab8402f680cb08c578451f
neutrons77 <justino.marcos@escolar.ifrn.edu.br>
2024-08-19 16:11:35 -0300

    scrip do player

---
+++ Meu Game 3D/Assets/player.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+{
+        float h- Input.GetAxis("Horizontal");
+        float v - Input.GetAxis("Vertical");
+        Vector3 direcao - new Vector3(h, 0, v);
+        rb.AddForce(direcao* velocidade Time.deltaTime, ForceMode.Impulse ):
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
