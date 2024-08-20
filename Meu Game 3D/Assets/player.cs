e7e7a7994235946ca031d75812b4d7be96cae8b5
neutrons77 <justino.marcos@escolar.ifrn.edu.br>
2024-08-20 14:30:21 -0300

    20/08

--- Meu Game 3D/Assets/player.cs
+++ Meu Game 3D/Assets/player.cs
@@ -4,24 +4,32 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    public int velocidade = 10;
+
+    private Rigibdbody rb;+.
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+
+    
+}
 
     // Update is called once per frame
     void Update()
     {
-        
+        float h =Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 direcao = new Vector3(h, 0, v);
+        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
     }
+
+
+        
 }
-{
-        float h- Input.GetAxis("Horizontal");
-        float v - Input.GetAxis("Vertical");
-        Vector3 direcao - new Vector3(h, 0, v);
-        rb.AddForce(direcao* velocidade Time.deltaTime, ForceMode.Impulse ):
-}
+
+
+
+
 
 
 
